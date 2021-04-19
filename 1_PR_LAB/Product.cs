1564d2fc8a4ba254ce4de995d984f7eb5b0f779d
Strijetsky <strijetsky@gmail.com>
2021-04-19 09:31:28 +0300

    Project created

---
+++ 1_PR_LAB/Product.cs
@@ -0,0 +1,41 @@
+﻿/**************************************************************
+--------------------------------------------------------------
+Класс Product.сs
+Группа П33
+Разработчик: Крыштанов М.А.
+Модифицирован: 19 апреля 2021 
+--------------------------------------------------------------
+***************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_PR_LAB
+{
+    class Product
+    {
+        private string Name;
+        private int Amount;
+        private float Cost;
+        public Product()
+        {
+            Name = "";
+            Amount = 0;
+            Cost = 0;
+        }
+        
+        public void ShowName()
+        {
+            Console.WriteLine("The current product name is {0}", Name);
+        }
+        public void ShowAmount()
+        {
+            Console.WriteLine("The current product amount = {0}", Amount);
+        }
+        public void ShowCost()
+        {
+            Console.WriteLine("The current product cost = {0}", Cost);
+        }
+    }
+}
